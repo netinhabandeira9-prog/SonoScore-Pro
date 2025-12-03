7e930bdbf76bdf69afae0acb50d2559e214a3360
netinhabandeira9-prog <netinhabandeira9@gmail.com>
2025-12-03 14:19:35 -0300

    Delete main.tsx

--- main.tsx
+++
@@ -1,9 +0,0 @@
-import { HelmetProvider } from 'react-helmet-async';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>
-)
